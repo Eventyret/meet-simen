431a272766c5be485a9905810c25bec24cf8aa03
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-17 17:01:38 +0000

    👌 IMPROVE: Styles and chalk added

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ welcome({
 });
 
 console.log(`
-${chalk(`Simen Daehlin`)}
+${chalk.bgBlue.bold(` imen Daehlin `)}
 
-Passionate about building state-of-the-art, easy to use, user-friendly websites and applications as I am, it's no surprise that I go out of my way to seek out new technologies and stay up-to-date on industry trends and advancements.
-Currently working as a backend developer at virgin media with Strapi as the choice of backend framework.
+${chalk.italic(`Passionate about building state-of-the-art, easy to use, user-friendly websites and applications as I am, it's no surprise that I go out of my way to seek out new technologies and stay up-to-date on industry trends and advancements.
+Currently working as a backend developer at virgin media with Strapi as the choice of backend framework.`)}
 
-🙋‍♂️ Website: https://dehlin.dev
-😎 Blog: https://blog.dehlin.dev
-🌏 LinkedIn: https://www.linkedin.com/in/simendaehlin/
-📖 Github: https://github.com/Eventyret
+🙋 ${chalk.hex('#da1884').inverse(` Website `)} ${chalk.dim(`https://dehlin.dev`)}
+😎 ${chalk.hex('#6937FF').inverse(` Blog `)}  ${chalk.dim(`https://blog.dehlin.dev`)}
+🌏 ${chalk.hex('#0077b5').inverse.bold(` LinkedIn `)}  ${chalk.dim(`https://www.linkedin.com/in/simendaehlin/`)}
+📖 ${chalk.hex('#6cc644').bold.inverse(` Website `)}  ${chalk.dim(`https://github.com/Eventyret`)}
 `);
