f8a2bc25b853e0a3b21617cbfa608ffd76100ccc
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-17 08:25:40 +0000

    📦 NEW: Added chalk dependancy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const pkgJson = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
 
 welcome({
   title: pkgJson.name,
@@ -19,8 +20,8 @@ Simen Daehlin
 Passionate about building state-of-the-art, easy to use, user-friendly websites and applications as I am, it's no surprise that I go out of my way to seek out new technologies and stay up-to-date on industry trends and advancements.
 Currently working as a backend developer at virgin media with Strapi as the choice of backend framework.
 
+🙋‍♂️ Website: https://dehlin.dev
+😎 Blog: https://blog.dehlin.dev
 🌏 LinkedIn: https://www.linkedin.com/in/simendaehlin/
 📖 Github: https://github.com/Eventyret
-😎 Blog: https://blog.dehlin.dev
-🙋‍♂️ Website: https://dehlin.dev
 `);
