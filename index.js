2a39f7e2ff62dbeff7e7bd9f37818472b7feb0d5
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-18 17:18:25 +0000

    👌 IMPROVE: Refactored string to data file

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
 #!/usr/bin/env node
-
-const chalk = require('chalk');
-const website = chalk.hex('#da1884').inverse;
-const blog = chalk.hex('#6937FF').inverse;
-const linkedIn = chalk.hex('#0077b5').inverse.bold;
-const github = chalk.hex('#6cc644').bold.inverse;
-const titleClr = chalk.hex('#FADC00').bold.inverse;
-const italic = chalk.italic;
-
 const init = require('./utils/init');
-const args = process.argv.slice(2);
-
-const dim = chalk.dim;
-
-const socialInfo = `
-🙋 ${website(` Website `)} ${dim(`https://dehlin.dev`)}
-😎 ${blog(` Blog `)}  ${dim(`https://blog.dehlin.dev`)}
-🌏 ${linkedIn(` LinkedIn `)}  ${dim(`https://www.linkedin.com/in/simendaehlin/`)}
-📖 ${github(` Github `)}  ${dim(`https://github.com/Eventyret`)}`;
-
-const printSocial = args.indexOf('--no-social') === -1;
-const social = printSocial ? socialInfo : '';
+const data = require('./utils/data');
 
 (async () => {
   init();
-  console.log(args);
-  console.log(`
-${titleClr(` Simen Daehlin `)}
-
-${italic(`Passionate about building state-of-the-art, easy to use, user-friendly websites and applications as I am, it's no surprise that I go out of my way to seek out new technologies and stay up-to-date on industry trends and advancements.
-Currently working as a backend developer at virgin media with Strapi as the choice of backend framework.`)}
-
-${social}
-`);
+  console.log(data.title);
+  console.log(data.bio);
+  console.log(data.social);
 })();
---
+++ utils/data.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+const website = chalk.hex('#da1884').inverse;
+const blog = chalk.hex('#6937FF').inverse;
+const linkedIn = chalk.hex('#0077b5').inverse.bold;
+const github = chalk.hex('#6cc644').bold.inverse;
+const titleClr = chalk.hex('#FADC00').bold.inverse;
+const dim = chalk.dim;
+const italic = chalk.italic;
+
+const title = `${titleClr(` Simen Daehlin `)}`;
+
+const bio = `${italic(`Passionate about building state-of-the-art, easy to use, user-friendly websites and applications as I am, it's no surprise that I go out of my way to seek out new technologies and stay up-to-date on industry trends and advancements.
+Currently working as a backend developer at virgin media with Strapi as the choice of backend framework.`)}`;
+
+const social = `
+🙋 ${website(` Website `)} ${dim(`https://dehlin.dev`)}
+😎 ${blog(` Blog `)}  ${dim(`https://blog.dehlin.dev`)}
+🌏 ${linkedIn(` LinkedIn `)}  ${dim(`https://www.linkedin.com/in/simendaehlin/`)}
+📖 ${github(` Github `)}  ${dim(`https://github.com/Eventyret`)}
+
+`;
+
+module.exports = {
+  title,
+  bio,
+  social,
+};
