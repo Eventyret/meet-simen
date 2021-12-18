65030fda82c066e30fc4d1022b170205ef663854
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-18 09:51:39 +0000

    Added new colors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ welcome({
 });
 
 console.log(`
-${chalk.bgBlue.bold(` Simen Daehlin `)}
+${chalk.hex('#FADC00').bold.inverse(` Simen Daehlin `)}
 
 ${chalk.italic(`Passionate about building state-of-the-art, easy to use, user-friendly websites and applications as I am, it's no surprise that I go out of my way to seek out new technologies and stay up-to-date on industry trends and advancements.
 Currently working as a backend developer at virgin media with Strapi as the choice of backend framework.`)}
