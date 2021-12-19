9c0fc467b244e3f2edc6881d78067eee520d9505
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-19 14:30:12 +0000

    🐛 FIX: Changed ora package to not use ESM

--- utils/stats.js
+++ utils/stats.js
@@ -1,10 +1,15 @@
+const ora = require('ora');
 const axios = require('axios');
 const apiURL = `https://api.github.com/users/eventyret`;
 const handleError = require('cli-handle-error');
 const to = require('await-to-js').default;
+
+const spinner = ora({ text: '' });
 module.exports = async () => {
+  spinner.start();
   const [error, res] = await to(axios.get(apiURL));
   const ghFollowers = res.data.followers;
+  spinner.stop();
   console.log(`Github Followers: ${ghFollowers}`);
   handleError(`API CALL FAILED`, error, false, false);
 };
