24ba3c65a9215a12937c4f540fcb4c22c272736b
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-19 13:48:23 +0000

    Added minimal to print

--- utils/init.js
+++ utils/init.js
@@ -1,22 +1,26 @@
 const welcome = require('cli-welcome');
 const checkNode = require('cli-check-node');
 const unhandled = require('cli-handle-unhandled');
+const boxen = require('boxen');
 
 const pkgJson = require('./../package.json');
 
-module.exports = (clear) => {
+module.exports = (minimal, clear) => {
   unhandled();
 
-  welcome({
-    title: pkgJson.name,
-    tagLine: `Howdy, nice to meet ya!`,
-    description: pkgJson.description,
-    version: pkgJson.version,
-    bgColor: '#FADC00',
-    color: '#000000',
-    bold: true,
-    clear,
-  });
+  !minimal &&
+    welcome({
+      title: pkgJson.name,
+      tagLine: `Howdy, nice to meet ya!`,
+      description: pkgJson.description,
+      version: pkgJson.version,
+      bgColor: '#FADC00',
+      color: '#000000',
+      bold: true,
+      clear,
+    });
+
+  minimal && console.log(`Simen Daehlin`);
 
   checkNode('10');
 };
