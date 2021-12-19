ba8da883cec49f9bb89b701993c31a60eefe1721
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-19 14:47:44 +0000

    📦 NEW: Added github repos and followers

--- utils/stats.js
+++ utils/stats.js
@@ -1,4 +1,5 @@
 const ora = require('ora');
+const { green, yellow } = require('chalk');
 const axios = require('axios');
 const apiURL = `https://api.github.com/users/eventyret`;
 const handleError = require('cli-handle-error');
@@ -6,10 +7,12 @@ const to = require('await-to-js').default;
 
 const spinner = ora({ text: '' });
 module.exports = async () => {
-  spinner.start();
+  spinner.start(`${yellow('FOLLOWERS')} 🦄 Finding unicorns on Github`);
   const [error, res] = await to(axios.get(apiURL));
-  const ghFollowers = res.data.followers;
+  const { public_repos: repos, followers } = res.data;
   spinner.stop();
-  console.log(`Github Followers: ${ghFollowers}`);
+  console.log(`Github Followers: ${followers}`);
+  console.log(`Github Repos: ${repos}`);
+  console.log();
   handleError(`API CALL FAILED`, error, false, false);
 };
