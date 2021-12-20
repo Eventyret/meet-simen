aeab35c4967ba9622410dd0459a14cb9e5deafd6
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2021-12-20 08:20:07 +0000

    👌 IMPROVE: Now passing a better readable help text

--- utils/cli.js
+++ utils/cli.js
@@ -1,60 +1,58 @@
 const meow = require('meow');
-const { green, yellow, cyan } = require('chalk');
+const meowHelper = require('cli-meow-help');
 
-const helpText = `
-  Usage
-    ${green(`npx meet-simen`)} ${yellow(`[--options]`)} ${cyan(`<command>`)}
-    
-  Options
-    ${yellow(`--no-social`)}     Don't print the social info
-    ${yellow(`--no-ad`)}         Don't print the special ad
-    ${yellow(`--no-clear`)}         Don't clear the console
-    ${yellow(`-d`)}, ${yellow(`--debug`)}     Print debug info
-    ${yellow(`-v`)}, ${yellow(`--version`)}   Print the version number
-    
-  Commands
-    ${cyan(`help`)}            Print CLI help information
-  
-    
-    
-  Examples
-  ${green(`npx meet-simen`)} ${yellow(`--no-social`)}
-  ${green(`npx meet-simen`)} -${yellow(`-no-ad`)}
-  ${green(`npx meet-simen`)} ${yellow(`--debug`)}
-  ${green(`npx meet-simen`)} ${cyan(`help`)}
+const flags = {
+  bio: {
+    type: 'boolean',
+    default: true,
+    desc: `Prints the bio`,
+  },
+  clear: {
+    type: 'boolean',
+    default: true,
+    desc: `Clears the terminal before printing the info`,
+  },
+  social: {
+    type: 'boolean',
+    default: true,
+    desc: `Prints the social media links`,
+  },
+  ad: {
+    type: 'boolean',
+    default: true,
+    desc: `Prints the ad`,
+  },
+  debug: {
+    type: 'boolean',
+    default: false,
+    alias: 'd',
+    desc: `Prints debug info`,
+  },
+  version: {
+    type: 'boolean',
+    default: false,
+    alias: 'v',
+    desc: `Prints the version`,
+  },
+};
 
-`;
+const commands = {
+  help: {
+    desc: `Prints this help info`,
+  },
+};
+
 const options = {
   inferType: true,
   hardRejection: false,
-  flags: {
-    bio: {
-      type: 'boolean',
-      default: true,
-      desc: `Prints the bio`,
-    },
-    clear: {
-      type: 'boolean',
-      default: true,
-    },
-    social: {
-      type: 'boolean',
-      default: true,
-    },
-    ad: {
-      type: 'boolean',
-      default: true,
-    },
-    debug: {
-      type: 'boolean',
-      default: false,
-      alias: 'd',
-    },
-    version: {
-      type: 'boolean',
-      default: false,
-      alias: 'v',
-    },
-  },
+  description: false,
+  flags,
 };
+
+const helpText = meowHelper({
+  name: 'npx meet-simen',
+  flags,
+  commands,
+});
+
 module.exports = meow(helpText, options);
